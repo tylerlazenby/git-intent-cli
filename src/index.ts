89fd50c89a8bfe87efee9dddf75181b1d93ea873
Tyler Lazenby <tyler.adam.lazenby@gmail.com>
2025-06-17 16:21:15 -0600

    [Essence Documentation] [Ethic Markdown] Created the readme file for explaination how to use the cli.

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,11 @@ program
     .requiredOption('--ethic <ethic>', 'The principle guiding the commit')
     .requiredOption('--expression <expression>', 'The actual change')
     .action(async ({ essence, ethic, expression }) => {
+        const plainMessage = `[Essence ${essence}] [Ethic ${ethic}] ${expression}`;
         const message = `[${chalk.dim('Essence')} ${essence}] [${chalk.dim('Ethic')} ${ethic}] ${expression}`;
         try {
             await git.add('.')
-            await git.commit(message);
+            await git.commit(plainMessage);
             console.log(chalk.bold(chalk.green('✅ Commit Created')), chalk.italic(message))
         } catch (err) {
             console.error(chalk.red('❌ Git commit failed:', err))
